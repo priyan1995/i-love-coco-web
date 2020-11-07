af36a41715fb450728e3e655886e863461f60805
Priyan Darshana <priyan@hyperspacehub.com>
2020-11-07 22:49:41 +0530

    about page development ongoing day end

--- wp-content/themes/ilovecoco/about.php
+++ wp-content/themes/ilovecoco/about.php
@@ -1,14 +1,69 @@
-<?php 
+<?php
 /*
 Template Name: About page
 */
 
 get_header();
-
-
 ?>
+<div class="pd-about-wrapper">
+
+    <section class="pd-about-banner-main">
+        <div class="container">
+            <div class="pd-breadcrumb text-center">
+                <h3>About Us</h3>
+                <a href="">Home</a><span>About Us</span>
+            </div>
+        </div>
+    </section>
+
+    <section class="pd-about-main-inf l-c-p-t-b-3">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4">
+                    <h2>I Love Coco</h2>
+                    <h4> Coconut product manufacturer & wholeseller.</h4>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam aperiam officiis, cupiditate magnam accusantium sit saepe deleniti deserunt numquam, consequuntur eum animi aspernatur.</p>
+                </div>
+                <div class="col-lg-4">
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda dolores quis amet, eaque ex tempora inventore? Dolores voluptas, autem exercitationem ea et deserunt suscipit, saepe neque fugiat blanditiis dicta laborum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates fugiat soluta magnam temporibus illum sapiente itaque, inventore excepturi commodi vero ea laborum repudiandae minus rerum facilis aut repellat ad autem.</p>
+                </div>
+                <div class="col-lg-4">
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda dolores quis amet, eaque ex tempora inventore? Dolores voluptas, autem exercitationem ea et deserunt suscipit, saepe neque fugiat blanditiis dicta laborum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates fugiat soluta magnam temporibus illum sapiente itaque, inventore excepturi commodi vero ea laborum repudiandae minus rerum facilis aut repellat ad autem.</p>
+                </div>
+            </div>
+        </div>
+    </section>
 
 
+    <section class="pd-vision-misison-sec l-c-p-t-b-3">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <h2>Vision</h2>
+                    <p class="pd-vision-para">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, veritatis eligendi fugiat animi cupiditate nemo odio veniam sequi quas quod unde fugit qui. Nostrum, officiis. Exercitationem quo veniam neque porro!</p>
+                </div>
+                <div class="col-lg-6">
+                    <h2>Mission</h2>
+                    <p class="pd-vision-para">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo inventore odit praesentium quae. Sapiente amet quas optio voluptatum, dolore cum sunt excepturi blanditiis consectetur modi animi tempora, necessitatibus deserunt cupiditate.</p>
+                </div>
+            </div>
+    
+        <div class="pd-direct-message-card">
+            <div class="row">
+                <div class="col-lg-4">
+                <img src="<?php echo bloginfo('template_url'); ?>/assets/img/ceo.png">
+                </div>
+                <div class="col-lg-8">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam minima quae inventore nisi quaerat assumenda vitae aspernatur architecto, dolore, praesentium, labore sint numquam earum error! Dolore repudiandae inventore minima neque.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam minima quae inventore nisi quaerat assumenda vitae aspernatur architecto, dolore, praesentium, labore sint numquam earum error! Dolore repudiandae inventore minima neque.</p>
+                </div>
+            </div>
+        </div>
+            
 
+
+        </div>
+    </section>
+
+</div>
 
 <?php get_footer();  ?>
