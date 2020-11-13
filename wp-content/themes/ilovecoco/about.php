ef936b16427f745fd87bc9632330055bb2d5a9cc
Priyan Darshana <priyan@hyperspacehub.com>
2020-11-13 14:45:22 +0530

    about page backend integration

--- wp-content/themes/ilovecoco/about.php
+++ wp-content/themes/ilovecoco/about.php
@@ -7,7 +7,7 @@ get_header();
 ?>
 <div class="pd-about-wrapper">
 
-    <section class="pd-about-banner-main pd-inner-banner" style="background-image:url('<?php echo bloginfo('template_url'); ?>/assets/img/abt-banner.png')">
+    <section class="pd-about-banner-main pd-inner-banner" style="background-image:url('<?php the_field('main_banner_abt'); ?>'">
         <div class="container">
             <div class="pd-breadcrumb text-center">
                 <h3>About Us</h3>
@@ -20,46 +20,46 @@ get_header();
         <div class="container">
             <div class="row">
                 <div class="col-lg-4">
-                    <h2>I Love Coco</h2>
-                    <h4> Coconut product manufacturer & wholeseller.</h4>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam aperiam officiis, cupiditate magnam accusantium sit saepe deleniti deserunt numquam, consequuntur eum animi aspernatur.</p>
+                    <h2><?php the_field('main_title_abt'); ?></h2>
+                    <h4><?php the_field('sub_title_abt'); ?></h4>
+                    <p><?php the_field('main_description_1'); ?></p>
                 </div>
                 <div class="col-lg-4">
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda dolores quis amet, eaque ex tempora inventore? Dolores voluptas, autem exercitationem ea et deserunt suscipit, saepe neque fugiat blanditiis dicta laborum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates fugiat soluta magnam temporibus illum sapiente itaque, inventore excepturi commodi vero ea laborum repudiandae minus rerum facilis aut repellat ad autem.</p>
+                    <p><?php the_field('main_description_2'); ?></p>
                 </div>
                 <div class="col-lg-4">
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda dolores quis amet, eaque ex tempora inventore? Dolores voluptas, autem exercitationem ea et deserunt suscipit, saepe neque fugiat blanditiis dicta laborum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates fugiat soluta magnam temporibus illum sapiente itaque, inventore excepturi commodi vero ea laborum repudiandae minus rerum facilis aut repellat ad autem.</p>
+                    <p><?php the_field('main_description_3'); ?></p>
                 </div>
             </div>
         </div>
     </section>
 
 
-    <section class="pd-vision-misison-sec l-c-p-t-b-3" style="background-image: url(<?php echo bloginfo('template_url'); ?>/assets/img/abt-back.png);">
+    <section class="pd-vision-misison-sec l-c-p-t-b-3" style="background-image: url(<?php  bloginfo('template_url'); ?>/assets/img/abt-back.png);">
         <div class="container">
             <div class="row">
                 <div class="col-lg-6">
                     <h2>Vision</h2>
-                    <p class="pd-vision-para">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, veritatis eligendi fugiat animi cupiditate nemo odio veniam sequi quas quod unde fugit qui. Nostrum, officiis. Exercitationem quo veniam neque porro!</p>
+                    <p class="pd-vision-para"><?php  the_field('vision_abt'); ?></p>
                 </div>
                 <div class="col-lg-6">
                     <h2>Mission</h2>
-                    <p class="pd-vision-para">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo inventore odit praesentium quae. Sapiente amet quas optio voluptatum, dolore cum sunt excepturi blanditiis consectetur modi animi tempora, necessitatibus deserunt cupiditate.</p>
+                    <p class="pd-vision-para"><?php  the_field('mission_abt'); ?></p>
                 </div>
             </div>
 
             <div class="pd-direct-message-card">
                 <div class="row">
                     <div class="col-lg-4">
-                        <img src="<?php echo bloginfo('template_url'); ?>/assets/img/ceo.png">
+                        <img src="<?php  the_field('directors_image_abt'); ?>">
                     </div>
                     <div class="col-lg-8">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam minima quae inventore nisi quaerat assumenda vitae aspernatur architecto, dolore, praesentium, labore sint numquam earum error! Dolore repudiandae inventore minima neque.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam minima quae inventore nisi quaerat assumenda vitae aspernatur architecto, dolore, praesentium, labore sint numquam earum error! Dolore repudiandae inventore minima neque.</p>
+                        <p><?php  the_field('directors_message_abt'); ?></p>
                         <br>
 
-                        <img src="<?php echo bloginfo('template_url'); ?>/assets/img/sign.png" style="margin-bottom: 10px;">
-                        <h5>Name Here</h5>
-                        <p>Managing Director - I Love Coco</p>
+                        <img src="<?php echo the_field('directors_signature_abt'); ?>" style="margin-bottom: 10px;">
+                        <h5><?php  the_field('directors_name_abt'); ?></h5>
+                        <p><?php the_field('directors_position_abt'); ?></p>
                     </div>
                 </div>
             </div>
@@ -71,29 +71,27 @@ get_header();
     <section class="pd-we-different-sec l-c-p-t-b-5">
         <div class="container">
 
-            <h2>Why We Are Different</h2>
-            <p class="main-para">Lorem ipsum dolor sit amet, consectetur</p>
+            <h2><?php  the_field('title_ww'); ?></h2>
+            <p class="main-para"><?php  the_field('description_ww'); ?></p>
 
             <!-- card -->
             <div class="row pd-aligned-center">
                 <div class="col-lg-6">
                     <div class="owl-carousel owl-theme" id="pd-abt-slider-1">
                         <div class="item">
-                            <img src="<?php echo bloginfo('template_url'); ?>/assets/img/abt-slider-1.png">
+                            <img src="<?php the_field('slider_image_1_abt_card_1'); ?>">
                         </div>
                         <div class="item">
-                            <img src="<?php echo bloginfo('template_url'); ?>/assets/img/abt-slider-1.png">
+                            <img src="<?php the_field('slider_image_2_abt_card_1'); ?>">
                         </div>
                         <div class="item">
-                            <img src="<?php echo bloginfo('template_url'); ?>/assets/img/abt-slider-1.png">
+                            <img src="<?php the_field('slider_image_3_abt_card_1'); ?>">
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-6">
-                    <h5>Text here</h5>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta quo animi omnis et sequi modi atque qui, magnam in ratione accusamus architecto nobis sed! Necessitatibus libero assumenda blanditiis voluptatibus tempora. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum delectus eaque velit quos, non est odio eligendi alias exercitationem eos illo, harum omnis quisquam dicta iure numquam consequatur dolorem tenetur!
-                        <br><br>
-                        Soluta quo animi omnis et sequi modi atque qui, magnam in ratione accusamus architecto nobis sed! Necessitatibus libero assumenda blanditiis voluptatibus tempora. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum delectus eaque velit quos, non est odio eligendi alias exercitationem eos illo, harum omnis quisquam dicta iure numquam consequatur dolorem tenetur!
+                    <h5><?php the_field('title_abt_card_1'); ?></h5>
+                    <p><?php the_field('description_abt_card_1'); ?>
                     </p>
 
                 </div>
@@ -102,24 +100,21 @@ get_header();
             <!-- card -->
             <div class="row pd-aligned-center">
                 <div class="col-lg-6">
-                    <h5>Text here</h5>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta quo animi omnis et sequi modi atque qui, magnam in ratione accusamus architecto nobis sed! Necessitatibus libero assumenda blanditiis voluptatibus tempora. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum delectus eaque velit quos, non est odio eligendi alias exercitationem eos illo, harum omnis quisquam dicta iure numquam consequatur dolorem tenetur!
-                        <br><br>
-                        Soluta quo animi omnis et sequi modi atque qui, magnam in ratione accusamus architecto nobis sed! Necessitatibus libero assumenda blanditiis voluptatibus tempora. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum delectus eaque velit quos, non est odio eligendi alias exercitationem eos illo, harum omnis quisquam dicta iure numquam consequatur dolorem tenetur!
-                    </p>
+                    <h5><?php the_field('title_abt_card_2'); ?></h5>
+                    <p><?php the_field('description_abt_card_2'); ?> </p>
 
                 </div>
 
                 <div class="col-lg-6">
                     <div class="owl-carousel owl-theme" id="pd-abt-slider-2">
                         <div class="item">
-                            <img src="<?php echo bloginfo('template_url'); ?>/assets/img/abt-slider-2.png">
+                            <img src="<?php the_field('slider_image_1_abt_card_2'); ?>">
                         </div>
                         <div class="item">
-                            <img src="<?php echo bloginfo('template_url'); ?>/assets/img/abt-slider-2.png">
+                            <img src="<?php the_field('slider_image_2_abt_card_2'); ?>">
                         </div>
                         <div class="item">
-                            <img src="<?php echo bloginfo('template_url'); ?>/assets/img/abt-slider-2.png">
+                            <img src="<?php the_field('slider_image_3_abt_card_2'); ?>">
                         </div>
                     </div>
                 </div>
@@ -131,22 +126,19 @@ get_header();
                 <div class="col-lg-6">
                     <div class="owl-carousel owl-theme" id="pd-abt-slider-3">
                         <div class="item">
-                            <img src="<?php echo bloginfo('template_url'); ?>/assets/img/abt-slider-3.png">
+                            <img src="<?php the_field('slider_image_1_abt_card_3'); ?>">
                         </div>
                         <div class="item">
-                            <img src="<?php echo bloginfo('template_url'); ?>/assets/img/abt-slider-3.png">
+                            <img src="<?php the_field('slider_image_2_abt_card_3'); ?>">
                         </div>
                         <div class="item">
-                            <img src="<?php echo bloginfo('template_url'); ?>/assets/img/abt-slider-3.png">
+                            <img src="<?php the_field('slider_image_3_abt_card_3'); ?>">
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-6">
-                    <h5>Text here</h5>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta quo animi omnis et sequi modi atque qui, magnam in ratione accusamus architecto nobis sed! Necessitatibus libero assumenda blanditiis voluptatibus tempora. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum delectus eaque velit quos, non est odio eligendi alias exercitationem eos illo, harum omnis quisquam dicta iure numquam consequatur dolorem tenetur!
-                        <br><br>
-                        Soluta quo animi omnis et sequi modi atque qui, magnam in ratione accusamus architecto nobis sed! Necessitatibus libero assumenda blanditiis voluptatibus tempora. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum delectus eaque velit quos, non est odio eligendi alias exercitationem eos illo, harum omnis quisquam dicta iure numquam consequatur dolorem tenetur!
-                    </p>
+                    <h5><?php the_field('title_abt_card_3'); ?></h5>
+                    <p><?php the_field('description_abt_card_3'); ?></p>
 
                 </div>
             </div>
@@ -155,11 +147,8 @@ get_header();
             <div class="row pd-aligned-center">
 
                 <div class="col-lg-6">
-                    <h5>Text here</h5>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta quo animi omnis et sequi modi atque qui, magnam in ratione accusamus architecto nobis sed! Necessitatibus libero assumenda blanditiis voluptatibus tempora. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum delectus eaque velit quos, non est odio eligendi alias exercitationem eos illo, harum omnis quisquam dicta iure numquam consequatur dolorem tenetur!
-                        <br><br>
-                        Soluta quo animi omnis et sequi modi atque qui, magnam in ratione accusamus architecto nobis sed! Necessitatibus libero assumenda blanditiis voluptatibus tempora. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum delectus eaque velit quos, non est odio eligendi alias exercitationem eos illo, harum omnis quisquam dicta iure numquam consequatur dolorem tenetur!
-                    </p>
+                    <h5><?php the_field('title_abt_card_4'); ?></h5>
+                    <p><?php the_field('description_abt_card_4'); ?></p>
 
                 </div>
 
@@ -167,13 +156,13 @@ get_header();
                 <div class="col-lg-6">
                     <div class="owl-carousel owl-theme" id="pd-abt-slider-4">
                         <div class="item">
-                            <img src="<?php echo bloginfo('template_url'); ?>/assets/img/abt-slider-4.png">
+                            <img src="<?php the_field('slider_image_1_abt_card_4'); ?>">
                         </div>
                         <div class="item">
-                            <img src="<?php echo bloginfo('template_url'); ?>/assets/img/abt-slider-4.png">
+                            <img src="<?php the_field('slider_image_2_abt_card_4'); ?>">
                         </div>
                         <div class="item">
-                            <img src="<?php echo bloginfo('template_url'); ?>/assets/img/abt-slider-4.png">
+                            <img src="<?php the_field('slider_image_4_abt_card_3'); ?>">
                         </div>
                     </div>
                 </div>
