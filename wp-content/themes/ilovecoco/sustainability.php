a92fb8aa923bc6db2bfbe1471ae4d4dbf11da820
Priyan Darshana <priyan@hyperspacehub.com>
2020-11-14 10:11:30 +0530

    CSR page development completed

--- wp-content/themes/ilovecoco/sustainability.php
+++ wp-content/themes/ilovecoco/sustainability.php
@@ -27,34 +27,211 @@ get_header();
 
 
             <div class="row">
-                <?php for ($x = 0; $x <= 1; $x++) { ?>
-                    <div class="col-lg-6 pd-pad-bot-30">
-                        <div class="pd-n-e-card">
+                <?php
+                $x = 0;
+                $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+                $FTA  = new WP_Query(array("post_type" => "fair_trade_activity", 'posts_per_page' => 2, 'paged' => $paged,));
+                if ($FTA->have_posts()) :
+                    while ($FTA->have_posts()) :
+                        $FTA->the_post();
 
-                            <div class="owl-carousel owl-theme" id="pd-fair-trade-slider<?php echo $x; ?>">
-                                <div class="item">
-                                    <img src="<?php echo bloginfo('template_url'); ?>/assets/img/n-e-post-1.png" class="pd-sustainability-img">
+                ?>
+
+
+                        <div class="col-lg-6 pd-pad-bot-30 pd-m-b-25">
+                            <div class="pd-n-e-card">
+
+                                <div class="owl-carousel owl-theme" id="pd-fair-trade-slider<?php echo $x; ?>">
+
+                                    <?php
+                                    $image_1_fta = get_field('image_1_fta');
+                                    $image_2_fta = get_field('image_2_fta');
+                                    $image_3_fta = get_field('image_3_fta');
+                                    $image_4_fta = get_field('image_4_fta');
+                                    $image_5_fta = get_field('image_5_fta');
+                                    ?>
+
+
+                                    <?php if ($image_1_fta) { ?>
+
+                                        <div class="item">
+                                            <img src="<?php echo $image_1_fta; ?>" class="pd-sustainability-img">
+                                        </div>
+
+                                    <?php } ?>
+
+                                    <?php if ($image_2_fta) { ?>
+                                        <div class="item">
+                                            <img src="<?php echo $image_2_fta; ?>" class="pd-sustainability-img">
+                                        </div>
+                                    <?php } ?>
+
+                                    <?php if ($image_3_fta) { ?>
+                                        <div class="item">
+                                            <img src="<?php echo $image_3_fta; ?>" class="pd-sustainability-img">
+                                        </div>
+                                    <?php } ?>
+
+                                    <?php if ($image_4_fta) { ?>
+                                        <div class="item">
+                                            <img src="<?php echo $image_4_fta; ?>" class="pd-sustainability-img">
+                                        </div>
+                                    <?php } ?>
+
+                                    <?php if ($image_5_fta) { ?>
+                                        <div class="item">
+                                            <img src="<?php echo $image_5_fta; ?>" class="pd-sustainability-img">
+                                        </div>
+                                    <?php } ?>
+
+
+
                                 </div>
-                                <div class="item">
-                                    <img src="<?php echo bloginfo('template_url'); ?>/assets/img/n-e-post-1.png" class="pd-sustainability-img">
-                                </div>
-                                <div class="item">
-                                    <img src="<?php echo bloginfo('template_url'); ?>/assets/img/n-e-post-1.png" class="pd-sustainability-img">
+
+
+                                <div class="pd-content-n-e">
+                                    <p>July 02, 2020 | <?php the_field('event_tag_fta'); ?></p>
+                                    <h4><?php the_title(); ?></h4>
+                                    <div class="pd-desc" style="margin-bottom: 20px;"><?php the_content(); ?></div>
+
                                 </div>
                             </div>
 
+                            <script>
+                                jQuery(document).ready(function($) {
+                                    $('#pd-fair-trade-slider<?php echo $x; ?>').owlCarousel({
+                                        loop: true,
+                                        margin: 5,
+                                        nav: true,
+                                        dots: false,
+                                        autoplay: false,
+                                        autoplayTimeout: 3000,
+                                        autoplayHoverPause: true,
+                                        responsive: {
+                                            0: {
+                                                items: 1
+                                            },
+                                            600: {
+                                                items: 1
+                                            },
+                                            1000: {
+                                                items: 1
+                                            }
+                                        }
+                                    });
+                                });
+                            </script>
+                        </div>
 
-                            <div class="pd-content-n-e">
-                                <p>July 02, 2020 | Lorem Ipsum</p>
-                                <h4>Healthy benifits of eating coconuts</h4>
-                                <p style="margin-bottom: 20px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, expedita enim repellendus in facilis officia aut consectetur eos modi repellat ipsum voluptas recusandae, exercitationem cum error rerum esse doloribus sunt.</p>
+                <?php
+                        $x++;
+                    endwhile;
+                endif;
+                ?>
+                <div class="col-lg-12">
+                    <?php
+                    $total_pages = $FTA->max_num_pages;
 
+                    if ($total_pages > 1) {
+
+                        $current_page = max(1, get_query_var('paged'));
+
+                        echo paginate_links(array(
+                            'base' => get_pagenum_link(1) . '%_%',
+                            'format' => '/page/%#%',
+                            'current' => $current_page,
+                            'total' => $total_pages,
+                            'prev_text'    => __('« prev'),
+                            'next_text'    => __('next »'),
+                        ));
+                    }
+
+                    wp_reset_query();
+                    ?>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="pd-f-t-section  l-c-p-t-b-5">
+        <div class="container">
+
+            <h2><?php the_field('main_title_02_csr'); ?></h2>
+            <p class="main-para"><?php the_field('sub_title_csr_2'); ?></p>
+            <p class="text-center pd-pad-bot-30"><?php the_field('main_description_02_csr'); ?></p>
+
+
+            <div class="row">
+                <?php
+                $y = 0;
+                $paged2 = (get_query_var('paged')) ? get_query_var('paged') : 1;
+                $csr  = new WP_Query(array("post_type" => "csr", 'posts_per_page' => 2, 'paged' => $paged2,));
+                if ($csr->have_posts()) :
+                    while ($csr->have_posts()) :
+                        $csr->the_post();
+
+                ?>
+
+
+                        <div class="col-lg-6 pd-pad-bot-30 pd-m-b-25">
+                            <div class="pd-n-e-card">
+                                <div class="owl-carousel owl-theme" id="pd-csr-slider<?php echo $y; ?>">
+
+                                    <?php
+                                    $image_1_csr = get_field('image_1_csr');
+                                    $image_2_csr = get_field('image_2_csr');
+                                    $image_3_csr = get_field('image_3_csr');
+                                    $image_4_csr = get_field('image_4_csr');
+                                    $image_5_csr = get_field('image_5_csr');
+                                    ?>
+
+
+                                    <?php if ($image_1_csr) { ?>
+
+                                        <div class="item">
+                                            <img src="<?php echo $image_1_csr; ?>" class="pd-sustainability-img">
+                                        </div>
+
+                                    <?php } ?>
+
+                                    <?php if ($image_2_csr) { ?>
+                                        <div class="item">
+                                            <img src="<?php echo $image_2_csr; ?>" class="pd-sustainability-img">
+                                        </div>
+                                    <?php } ?>
+
+                                    <?php if ($image_3_csr) { ?>
+                                        <div class="item">
+                                            <img src="<?php echo $image_3_csr; ?>" class="pd-sustainability-img">
+                                        </div>
+                                    <?php } ?>
+
+                                    <?php if ($image_4_csr) { ?>
+                                        <div class="item">
+                                            <img src="<?php echo $image_4_csr; ?>" class="pd-sustainability-img">
+                                        </div>
+                                    <?php } ?>
+
+                                    <?php if ($image_5_csr) { ?>
+                                        <div class="item">
+                                            <img src="<?php echo $image_5_csr; ?>" class="pd-sustainability-img">
+                                        </div>
+                                    <?php } ?>
+
+
+                                </div>
+                                <div class="pd-content-n-e">
+                                    <p>July 02, 2020 | <?php the_field('event_tag_csr'); ?></p>
+                                    <h4><?php the_title(); ?></h4>
+                                    <div class="pd-desc" style="margin-bottom: 20px;"><?php the_content(); ?></div>
+
+                                </div>
                             </div>
                         </div>
 
                         <script>
                             jQuery(document).ready(function($) {
-                                $('#pd-fair-trade-slider<?php echo $x; ?>').owlCarousel({
+                                $('#pd-csr-slider<?php echo $y; ?>').owlCarousel({
                                     loop: true,
                                     margin: 5,
                                     nav: true,
@@ -76,76 +253,37 @@ get_header();
                                 });
                             });
                         </script>
-                    </div>
 
-                <?php } ?>
+                <?php
+                        $y++;
+                    endwhile;
+                endif;
 
-            </div>
-    </section>
+                ?>
+                <div class="col-lg-12">
+                    <?php
+                    $total_pages2 = $csr->max_num_pages;
 
-    <section class="pd-f-t-section  l-c-p-t-b-5">
-        <div class="container">
+                    if ($total_pages2 > 1) {
 
-            <h2><?php the_field('main_title_02_csr'); ?></h2>
-            <p class="main-para"><?php the_field('sub_title_csr_2'); ?></p>
-            <p class="text-center pd-pad-bot-30"><?php the_field('main_description_02_csr'); ?></p>
+                        $current_page2 = max(1, get_query_var('paged'));
 
+                        echo paginate_links(array(
+                            'base' => get_pagenum_link(1) . '%_%',
+                            'format' => '/page/%#%',
+                            'current' => $current_page2,
+                            'total' => $total_pages2,
+                            'prev_text'    => __('« prev'),
+                            'next_text'    => __('next »'),
+                        ));
+                    }
+                    wp_reset_query();
+                    ?>
 
-            <div class="row">
-                <?php for ($x = 0; $x <= 3; $x++) { ?>
-                    <div class="col-lg-6 pd-pad-bot-30">
-                        <div class="pd-n-e-card">
-                            <div class="owl-carousel owl-theme" id="pd-csr-slider<?php echo $x; ?>">
-                                <div class="item">
-                                    <img src="<?php echo bloginfo('template_url'); ?>/assets/img/n-e-post-1.png" class="pd-sustainability-img">
-                                </div>
-                                <div class="item">
-                                    <img src="<?php echo bloginfo('template_url'); ?>/assets/img/n-e-post-1.png" class="pd-sustainability-img">
-                                </div>
-                                <div class="item">
-                                    <img src="<?php echo bloginfo('template_url'); ?>/assets/img/n-e-post-1.png" class="pd-sustainability-img">
-                                </div>
-                            </div>
-                            <div class="pd-content-n-e">
-                                <p>July 02, 2020 | Lorem Ipsum</p>
-                                <h4>Healthy benifits of eating coconuts</h4>
-                                <p style="margin-bottom: 20px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, expedita enim repellendus in facilis officia aut consectetur eos modi repellat ipsum voluptas recusandae, exercitationem cum error rerum esse doloribus sunt.</p>
-
-                            </div>
-                        </div>
-                    </div>
-
-                    <script>
-                        jQuery(document).ready(function($) {
-                            $('#pd-csr-slider<?php echo $x; ?>').owlCarousel({
-                                loop: true,
-                                margin: 5,
-                                nav: true,
-                                dots: false,
-                                autoplay: false,
-                                autoplayTimeout: 3000,
-                                autoplayHoverPause: true,
-                                responsive: {
-                                    0: {
-                                        items: 1
-                                    },
-                                    600: {
-                                        items: 1
-                                    },
-                                    1000: {
-                                        items: 1
-                                    }
-                                }
-                            });
-                        });
-                    </script>
-
-                <?php } ?>
-
-
-
+                </div>
 
             </div>
+        </div>
     </section>
 
 
