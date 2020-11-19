e07ac6b9e764437e581e05b75449d50c6755f1f2
Priyan Darshana <priyan@hyperspacehub.com>
2020-11-20 00:13:56 +0530

    Quality page development completed

--- wp-content/themes/ilovecoco/assets/js/main.js
+++ wp-content/themes/ilovecoco/assets/js/main.js
@@ -24,6 +24,28 @@ jQuery(document).ready(function ($) {
         }
     });
 
+    $('#pd-mds-slider').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: true,
+        dots: false,
+        animateOut: 'fadeOut',
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+
     $('#pd-cert-slider').owlCarousel({
         loop: true,
         margin: 0,
@@ -45,6 +67,30 @@ jQuery(document).ready(function ($) {
             }
         }
     });
+
+
+    $('#pd-cert-slider-quality').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: true,
+        dots: false,
+        animateOut: 'fadeOut',
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
+
 
     $('#pd-n-e-slider').owlCarousel({
         loop: true,
@@ -194,6 +240,26 @@ jQuery(document).ready(function ($) {
     });
 
     
+    $('#pd-mid-green-slider').owlCarousel({
+        loop: true,
+        margin: 5,
+        nav: true,
+        dots: false,
+        autoplay: false,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
 
 
 
---
+++ wp-content/themes/ilovecoco/quality.php
@@ -0,0 +1,231 @@
+<?php
+/*
+Template Name: Quality page
+*/
+
+get_header();
+?>
+
+
+<section class="pd-about-banner-main pd-inner-banner" style="background-image:url('<?php the_field('banner_image'); ?>'">
+    <div class="container">
+        <div class="pd-breadcrumb text-center">
+            <h3 data-aos="fade-up" data-aos-duration="500">Quality</h3>
+            <a href="<?php echo site_url() ?>" data-aos="fade-up" data-aos-duration="800">Home / </a><span data-aos="fade-up" data-aos-duration="500">Quantity</span>
+        </div>
+    </div>
+</section>
+
+
+<section class="pd-quality-main-cont l-c-abt-sec l-c-p-t-b-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6" data-aos="fade-up" data-aos-duration="500">
+                <div class="owl-carousel owl-theme" id="pd-mds-slider">
+
+                    <?php
+
+                    $image1 = get_field('image_1_mds');
+                    $image2 = get_field('image_2_mds');
+                    $image3 = get_field('image_3_mds');
+
+                    ?>
+
+                    <?php if ($image1) { ?>
+                        <div class="item">
+                            <img src="<?php echo $image1; ?>">
+                        </div>
+                    <?php } ?>
+
+                    <?php if ($image2) { ?>
+                        <div class="item">
+                            <img src="<?php echo $image2; ?>">
+                        </div>
+                    <?php } ?>
+
+                    <?php if ($image3) { ?>
+                        <div class="item">
+                            <img src="<?php echo $image3; ?>">
+                        </div>
+                    <?php } ?>
+
+                </div>
+                <p data-aos="fade-up" data-aos-duration="1500"><?php the_field('description_3'); ?></p>
+            </div>
+            <div class="col-lg-6">
+                <h3 data-aos="fade-up" data-aos-duration="500"><?php the_field('Main_title_quality') ?></h3>
+                <h4 data-aos="fade-up" data-aos-duration="800"><?php the_field('sub_title_quality'); ?></h4>
+                <p data-aos="fade-up" data-aos-duration="1500"><?php the_field('description_1'); ?></p>
+                <p data-aos="fade-up" data-aos-duration="1500"><?php the_field('description_2'); ?></p>
+                <br>
+                <div class="owl-carousel owl-theme" id="pd-cert-slider-quality">
+
+                    <?php
+                    $certificates  = new WP_Query(array("post_type" => "certifications", "order" => "DESC"));
+                    if ($certificates->have_posts()) :
+                        while ($certificates->have_posts()) :
+                            $certificates->the_post();
+
+                    ?>
+
+                            <div class="item text-center">
+                                <div class="pd-cert-card">
+                                    <img src="<?php the_field('image_cert'); ?>">
+                                </div>
+                            </div>
+
+                    <?php
+                        endwhile;
+                    endif;
+                    wp_reset_query();
+                    ?>
+
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+
+</section>
+
+
+<section class="pd-mid-green  l-c-p-t-b-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 pd-m-b-20">
+                <div class="owl-carousel owl-theme" id="pd-mid-green-slider">
+
+                    <?php
+
+                    $image1 = get_field('slider_image_mgs1');
+                    $image2 = get_field('slider_image_mgs2');
+                    $image3 = get_field('slider_image_mgs3');
+
+                    ?>
+
+                    <?php if ($image1) { ?>
+                        <div class="item">
+                            <img src="<?php echo $image1; ?>">
+                        </div>
+                    <?php } ?>
+
+                    <?php if ($image2) { ?>
+                        <div class="item">
+                            <img src="<?php echo $image2; ?>">
+                        </div>
+                    <?php } ?>
+
+                    <?php if ($image3) { ?>
+                        <div class="item">
+                            <img src="<?php echo $image3; ?>">
+                        </div>
+                    <?php } ?>
+
+                </div>
+            </div>
+            <div class="col-lg-6 pd-m-b-20">
+                <div><?php the_field('right_content_mgs'); ?></div>
+            </div>
+
+            <div class="col-lg-6">
+                <div><?php the_field('left_content_mgs'); ?></div>
+            </div>
+
+            <div class="col-lg-6">
+                <div class="pd-mid-green-img-t-1">
+                    <img src="<?php the_field('right_sided_image_1_mgs'); ?>" class="w-100">
+                </div>
+                <div class="pd-flex">
+                    <div class="pd-mid-green-img-b-1">
+                        <img src="<?php the_field('right_sided_image_2_mgs'); ?>">
+                    </div>
+                    <div class="pd-mid-green-img-b-2">
+                        <img src="<?php the_field('right_sided_image_3_mgs'); ?>">
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
+    </div>
+</section>
+
+
+<section class="pd-quality-cert-sec  l-c-p-t-b-5">
+    <div class="container">
+        <h2 data-aos="fade-up" data-aos-duration="500" class="text-center"><?php the_field('title_certificate') ?></h2>
+        <h4 data-aos="fade-up" data-aos-duration="800" class="text-center"><?php the_field('subtitle_certificate'); ?></h4>
+        <div data-aos="fade-up" data-aos-duration="1500" style="margin-bottom: 20px;"><?php the_field('content_certificate_sec'); ?></div>
+
+        <div class="row">
+
+
+            <?php
+            $z = 500;
+            $y = 1;
+            $paged2 = (get_query_var('paged')) ? get_query_var('paged') : 1;
+            $cert  = new WP_Query(array("post_type" => "certificates", 'posts_per_page' => 4, 'paged' => $paged2,));
+            if ($cert->have_posts()) :
+                while ($cert->have_posts()) :
+                    $cert->the_post();
+
+            ?>
+
+                    <div class="pd-cert-card-2 <?php if ($y%2==1){ echo 'right-aligned-cert'; }else{ echo 'left-aligned-cert'; } ?>  ">
+                        <h5><?php the_title() ?></h5>
+                        <div><?php the_content(); ?></div>
+                        <img src="<?php the_field('image_certificate'); ?>">
+                    </div>
+
+                    <!-- <div class="pd-cert-card left-aligned-cert">
+                        <h5>lorem ipsum</h5>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab itaque in at laudantium alias. Optio sint quo nulla obcaecati, consectetur eum, repellat earum eius sed dolore esse asperiores fugiat autem.</p>
+                        <img src="http://localhost/ilovecoco/wp-content/uploads/2020/11/unnamed.jpg">
+                    </div> -->
+
+            <?php
+                    $z = $z + 500;
+                    $y++;
+                endwhile;
+            endif;
+
+            ?>
+
+            <div class="col-lg-12">
+                <?php
+                $total_pages2 = $cert->max_num_pages;
+
+                if ($total_pages2 > 1) {
+
+                    $current_page2 = max(1, get_query_var('paged'));
+
+                    echo paginate_links(array(
+                        'base' => get_pagenum_link(1) . '%_%',
+                        'format' => '/page/%#%',
+                        'current' => $current_page2,
+                        'total' => $total_pages2,
+                        'prev_text'    => __('« prev'),
+                        'next_text'    => __('next »'),
+                    ));
+                }
+                wp_reset_query();
+                ?>
+
+            </div>
+
+
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+
+
+
+<?php get_footer(); ?>
